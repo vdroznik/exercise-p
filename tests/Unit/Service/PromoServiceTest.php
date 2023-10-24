c9de40f2fc497e5e7a9d8efe4173d29d13f53183
Vladimir Droznik <vladimir.droznik@gmail.com>
2023-10-24 18:09:04 +0300

    added algorithmic promo codes generator with unit tests

---
+++ tests/Unit/Service/PromoServiceTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Service;
+
+use ExercisePromo\Service\PromoService;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Random\Engine as RandomEngine;
+
+class PromoServiceTest extends TestCase
+{
+    private PromoService $promoService;
+
+    protected function setUp(): void
+    {
+        $this->promoService = new PromoService(
+            new class implements RandomEngine {
+                public function generate(): string
+                {
+                    return "42";
+                }
+            });
+    }
+    
+    #[DataProvider('userIdsProvider')]
+    public function testGeneratePromoForUser($userId, $expectedPromo)
+    {
+        $promo = $this->promoService->generatePromoForUser($userId);
+        $this->assertEquals($expectedPromo, $promo, 'Promo code is correct for user_id = 2');
+    }
+
+    #[DataProvider('userIdsProvider')]
+    public function testReversePromoToUserId($expectedUserId, $promo)
+    {
+        // below is the reversing algorithm which reconstructs userId from promo code
+        // zero byte is added for the base64_decode to return 8 bytes, required by unpack("Q")
+        $restored = unpack("Q", base64_decode($promo) . "\0")[1];
+        $restoredBin = str_pad(decbin($restored), 56, '0', STR_PAD_LEFT);
+        $resultBin = '';
+        for ($i = 0; $i < 24; $i++) {
+            $resultBin .= $restoredBin[55-$i*2];
+        }
+        for ($i = 24; $i < 32; $i++) {
+            $resultBin .= $restoredBin[31-$i];
+        }
+        $resn = bindec($resultBin);
+
+        $this->assertEquals($expectedUserId, $resn);
+    }
+
+    public static function userIdsProvider()
+    {
+        return [
+            'small id with many binary zeroes' => [2, 'ICIgIiAiQA'],
+            'alternating binary 101010' => [2_863_311_530, 'MTMxMzEzVQ'],
+            'maxint, all binary ones' => [4_294_967_295, 'dXd1d3V3/w']
+        ];
+    }
+}
