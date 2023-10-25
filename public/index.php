12bd5cc9ab60a9eb4de11de773008529cc3488ec
Vladimir Droznik <vladimir.droznik@gmail.com>
2023-10-25 21:56:35 +0300

    refactored profile controller

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\DriverManager;
 use ExercisePromo\Controller\FrontController;
 use ExercisePromo\Controller\AuthController;
 use ExercisePromo\Controller\ProfileController;
+use ExercisePromo\Controller\PromoController;
 use ExercisePromo\Middleware\AuthMiddleware;
 use Odan\Session\Middleware\SessionStartMiddleware;
 use Odan\Session\PhpSession;
@@ -15,6 +16,7 @@ use Psr\Container\ContainerInterface;
 use Random\Engine as RandomEngine;
 use Random\Engine\Secure as RandomEngineSecure;
 use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
 use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -57,10 +59,10 @@ $app->get('/', [FrontController::class, 'index']);
 $app->post('/check-login', [AuthController::class, 'checkLogin'])
     ->add(SessionStartMiddleware::class);
 
-$app->group('/profile', function () use ($app) {
-    $app->get('/profile', [ProfileController::class, 'index']);
-    $app->get('/profile/getpromo', [ProfileController::class, 'getPromo']);
-    $app->get('/profile/logout', [AuthController::class, 'logout']);
+$app->group('/profile', function (RouteCollectorProxy $group) {
+    $group->get('', [ProfileController::class, 'index']);
+    $group->get('/getpromo', [PromoController::class, 'getPromo']);
+    $group->get('/logout', [AuthController::class, 'logout']);
 })->add(AuthMiddleware::class)
     ->add(SessionStartMiddleware::class);
 
---
+++ src/Controller/PromoController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ExercisePromo\Controller;
+
+use ExercisePromo\Auth\Auth;
+use ExercisePromo\Repository\IpsRepository;
+use ExercisePromo\Service\PromoService;
+use Odan\Session\SessionInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+readonly class PromoController
+{
+    private const MAX_PROMOS_PER_IP = 1000;
+
+    public function __construct(
+        private Auth $auth,
+        private PromoService $promoService,
+        private IpsRepository $ipsRepo,
+        private SessionInterface $session,
+    ) {}
+
+    public function getPromo(Request $request, Response $response): Response
+    {
+        $ip = $request->getServerParams()['REMOTE_ADDR'];
+        $ipPacked = ip2long($ip);
+
+        $cnt = $this->ipsRepo->getCountByIp($ipPacked);
+        if ($cnt > self::MAX_PROMOS_PER_IP) {
+            $flash = $this->session->getFlash();
+            $flash->add('error', 'The number of promo codes issued for this ip is too high.');
+
+            return $response
+                ->withHeader('Location', '/profile')
+                ->withStatus(302);
+        }
+
+        $promo = $this->promoService->findOrCreatePromoForUser(
+            $this->auth->getUser(),
+            $ipPacked
+        );
+
+        return $response
+            ->withHeader('Location', 'https://www.google.com/?query=' . $promo->code)
+            ->withStatus(302);
+    }
+}
